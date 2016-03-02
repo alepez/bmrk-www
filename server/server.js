659ef73e7d064ef96675a51f0562c6006fe4e747
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-03-02 22:15:52 +0100

    test server

---
+++ server/server.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var uuid = require('node-uuid');
+var app = express();
+
+var bookmarksFile = path.join(__dirname, 'bookmarks.json');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+app.get('/bookmarks', function(req, res) {
+  fs.readFile(bookmarksFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/bookmarks', function(req, res) {
+  fs.readFile(bookmarksFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var bookmarks = JSON.parse(data);
+    var newBookmark = {
+      id: uuid.v4(),
+      url: req.body.url,
+      title: req.body.title,
+      description: req.body.description,
+      tags: req.body.tags,
+      user: req.body.user,
+      notes: req.body.notes
+    };
+    bookmarks.push(newBookmark);
+    fs.writeFile(bookmarksFile, JSON.stringify(bookmarks, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(bookmarks);
+    });
+  });
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
