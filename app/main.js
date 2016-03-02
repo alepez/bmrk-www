46d74113ec8c88f8e116dc7c23f4fd14de2af313
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-03-03 00:06:03 +0100

    bookmark list

--- app/main.js
+++ app/main.js
@@ -1,8 +1,29 @@
-// main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Bookmark= React.createClass({
+  render: function() {
+    return (
+      <div className="bookmark">
+        <span class="title">{this.props.title}</span>
+      </div>
+    );
+  }
+});
+
+var BookmarkList = React.createClass({
+  render: function() {
+    return (
+      <div className="bookmarkList">
+        <Bookmark title="Alessandro Pezzato Blog" />
+        <Bookmark title="Google" />
+      </div>
+    );
+  }
+});
+
+
 ReactDOM.render(
-  <h1>Hello, Chtulhu!!!</h1>,
-  document.getElementById('example')
+  <BookmarkList/>,
+  document.getElementById('bmrk-root')
 );
