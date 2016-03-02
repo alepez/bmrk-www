35190eaa64a653893f4c85a227b01cd70e00dea2
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-03-02 23:34:08 +0100

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var source = require('vinyl-source-stream');
+var gulp = require('gulp');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+
+var scriptsDir = './app';
+var buildDir = './server/public';
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  console.log(args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
+
+function buildScript(file, watch) {
+  var props = {
+    entries: [scriptsDir + '/' + file],
+    debug: true,
+    cache: {},
+    packageCache: {},
+  };
+
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+  bundler.transform(babelify, {
+    'presets': ['react']
+  });
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream.on('error', console.log.bind(console))
+      .pipe(source(file))
+      .pipe(gulp.dest(buildDir + '/'));
+  }
+
+  bundler.on('update', function () {
+    rebundle();
+    console.log('rebundle');
+  });
+
+  return rebundle();
+}
+
+gulp.task('build', function () {
+  return buildScript('main.js', false);
+});
+
+gulp.task('watch', ['build'], function () {
+  return buildScript('main.js', true);
+});
