8cfec3fd3267387060954d48d5a35d37b4fee317
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-03-03 00:19:07 +0100

    props

--- app/main.js
+++ app/main.js
@@ -5,7 +5,9 @@ var Bookmark= React.createClass({
   render: function() {
     return (
       <div className="bookmark">
-        <span class="title">{this.props.title}</span>
+        <span className="title">{this.props.title}</span>
+        <span className="url">{this.props.url}</span>
+        <span className="description">{this.props.description}</span>
       </div>
     );
   }
@@ -13,17 +15,38 @@ var Bookmark= React.createClass({
 
 var BookmarkList = React.createClass({
   render: function() {
+    var items  = this.props.data.map(function(item) {
+      return (
+        <Bookmark
+          title={item.title}
+          url={item.url}
+          description={item.description}
+          key={item.id}
+        />
+      );
+    });
     return (
       <div className="bookmarkList">
-        <Bookmark title="Alessandro Pezzato Blog" />
-        <Bookmark title="Google" />
+        {items}
       </div>
     );
   }
 });
 
+var data = [
+  {
+    "id": "8586188a-3e72-4cec-8513-4af02d83be75",
+    "url": "http://pezzato.net",
+    "title": "Alessandro Pezzato Blog",
+    "description": "A blog about stuff",
+    "tags": ["alessandro", "pezzato", "alepez"],
+    "user": "alepez",
+    "notes": "Lorem ipsum dolor sit amet."
+  }
+]
+
 
 ReactDOM.render(
-  <BookmarkList/>,
+  <BookmarkList data={data}/>,
   document.getElementById('bmrk-root')
 );
