04400b318392682d93eae4b6a617f95641f4310c
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-03-03 02:44:39 +0100

    form with handlers

--- app/components/new-bookmark-form.js
+++ app/components/new-bookmark-form.js
@@ -1,16 +1,72 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  handleUrlChange: function(e) {
+    this.setState({url: e.target.value});
+  },
+  handleTitleChange: function(e) {
+    this.setState({title: e.target.value});
+  },
+  handleDescriptionChange: function(e) {
+    this.setState({description: e.target.value});
+  },
+  handleTagsChange: function(e) {
+    this.setState({tags: e.target.value});
+  },
+  handleNotesChange: function(e) {
+    this.setState({notes: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    console.log(this.state);
+  },
   render: function() {
     return (
       <div className="newBookmarkForm clearfix">
-        <form>
-          <input className="textField" type="text" placeholder="url" />
-          <input className="textField" type="text" placeholder="title" />
-          <input className="textField" type="text" placeholder="description" />
-          <input className="textField" type="text" placeholder="tags" />
-          <textarea className="textArea" placeholder="notes"></textarea>
-          <input className="submitBtn" type="submit" value="Add" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="textField"
+            type="text"
+            placeholder="url"
+            value={this.state.url}
+            onChange={this.handleUrlChange}
+          />
+          <input
+            className="textField"
+            type="text"
+            placeholder="title"
+            value={this.state.title}
+            onChange={this.handleTitleChange}
+          />
+          <input
+            className="textField"
+            type="text"
+            placeholder="description"
+            value={this.state.description}
+            onChange={this.handleDescriptionChange}
+          />
+          <input
+            className="textField"
+            type="text"
+            placeholder="tags"
+            value={this.state.tags}
+            onChange={this.handleTagsChange}
+          />
+          <textarea
+            className="textArea"
+            placeholder="notes"
+            value={this.state.notes}
+            onChange={this.handleNotesChange}>
+          </textarea>
+
+          <input
+            className="submitBtn"
+            type="submit"
+            value="Add"
+          />
         </form>
       </div>
     );
